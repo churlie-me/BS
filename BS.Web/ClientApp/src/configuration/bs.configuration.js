1ec4134778d4ae96fa171ebb883a160ced7be069
Charles Aguma <chalesaguma@gmail.com>
2021-05-02 00:54:02 +0300

    Hide active apis

--- BS.Web/ClientApp/src/configuration/bs.configuration.js
+++ BS.Web/ClientApp/src/configuration/bs.configuration.js
@@ -1,6 +1,6 @@
 export class BSConfiguration {  
-  //REST_API_SERVICE = "http://coupcoupe.prestofide.be/service/api";
+  //REST_API_SERVICE = "http://xxxxxx.xxxxxxx.be/service/api";
   //REST_API_SERVICE = "http://localhost/bs.api/api"
-  REST_API_SERVICE = "http://api.admin.infinimo.be/api"
+  REST_API_SERVICE = "http://xxx.xxxxx.xxxxxxx.be/api"
   //public headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json', 'Accept': 'application/json' });
 }
